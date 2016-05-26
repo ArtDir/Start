851f0f809fdc60621dddd84e7318648cf2797b0e
ArtDir <effest@gmail.com>
2016-05-26 22:12:08 +0300

    Cool checkboxes
    Добавил Данилы красивые чекбоксы

--- js/common.js
+++ js/common.js
@@ -81,46 +81,5 @@ $(document).ready(function() {
 		});
 		return false;
 	});
-	//Радиокнопки и чекбоксы от Дани
-		$(".custom_input").each(function(index, el) {
-		var label = $(this);
-		var input = $("input", label);
-		$("<div class='checker'></div>").appendTo(label);
-		var cheker = $("div.custom_input", label);
-
-
-		if(label.hasClass('active')){
-			input.trigger('click');
-		}
-		switch(input.attr("type")){
-			case "radio":
-				label.addClass('radio');
-				if(label.hasClass('disabled')){
-					return;
-				}
-				input.click(function(event) {
-					if(label.hasClass('active')){
-						label.removeClass('active');
-						input.attr('checked', false);
-					}
-				});
-				input.change(function(event) {
-					label.addClass('active').siblings().removeClass('active');
-				});
-				break
-			case "checkbox":
-				label.addClass('checkbox');
-				if(label.hasClass('disabled')){
-					return;
-				}
-				input.change(function(event) {
-					if(label.hasClass('disabled')){
-						input.removeAttr('checked');
-					}else{
-						label.toggleClass('active');
-					}
-				});
-				break
-		}
-	});
+	
 });
